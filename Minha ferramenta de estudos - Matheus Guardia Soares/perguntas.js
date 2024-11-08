0cbc4aa4ec67fdabf954cccb80ab2635cc640fa0
2CITMg27 <00001093233527sp@al.educacao.sp.gov.br>
2024-11-08 11:33:02 -0300

    Add files via upload

---
+++ Minha ferramenta de estudos - Matheus Guardia Soares/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é LuaScript?',
+    'O LuaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Japão?',
+    'A capital do Japão é Tokio'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz maçã em Inglês?',
+    'Maçã em ingles é APPLE (ÉPOU)'
+)
